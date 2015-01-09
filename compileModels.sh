0f2a0a2ec162fd7fd6eabef8a40369296877cb2f
Nick Tate <ntate22@gmail.com>
2015-01-09 12:41:02 -0500

    added base models

--- compileModels.sh
+++ compileModels.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-if [ ! -f /protobuf ]; then
+if [ -f /protobuf ]; then
   echo "Protobuf not installed...fetching from internet"
   git clone git@github.com:google/protobuf.git
   echo "Installing protobuf..."
   cd protobuf && ./autogen.sh && ./configure && make && make install && cd ..
 fi
 
-PROTOS=./models/*.proto
+PROTOS=./models/proto/*.proto
 
 for proto in $PROTOS
 do
@@ -16,7 +15,7 @@ do
 
   desc=${proto##*/}
   desc=${desc%.*}
-  desc=./models/$desc.desc
+  desc=./models/desc/$desc.desc
 
   ./protobuf/src/protoc --descriptor_set_out=$desc --include_imports $proto
 done
